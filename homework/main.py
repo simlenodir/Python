f9950bd743eb8b72c6850def00e0b3c6a7af8309
simlenodir <nodirsmailov0@gmail.com>
2025-03-22 14:45:17 +0500

    lesson 1 python lessons started
    lesson 1 python lessons started

---
+++ homework/main.py
@@ -0,0 +1,29 @@
+print('Lesson 1 started')
+
+#  Task 1
+a = 5 
+p = a * 4
+s = a * a
+print(p, s)
+
+# Task 2
+
+d = 4
+p = d * 3.14
+print (p)
+
+# Task 3
+
+a = 6 
+b = 4
+s = (a+b) / 2 
+print(s)
+
+# Task 4
+
+a = 4
+b = 9
+sum = a + b
+product = a * b
+square_a = a * a
+square_b = b * b
